f24d6b16ea340d30872094c3a743ac314e28f7ed
YoussefAhmed365 <youssef.ahmed5002@gmail.com>
2026-02-05 19:46:36 +0200

    Playing songs in background

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,13 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
-  // 1. External Services (External Libraries)
+  // تسجيل مشغل الصوت كـ Lazy Singleton (نسخة واحدة تظل موجودة بالذاكرة)
   getIt.registerLazySingleton<AudioPlayer>(() => AudioPlayer());
 
-  // هنا سنقوم لاحقاً بتسجيل الـ AudioHandler والـ Repositories
-  // مثال (سيتم تفعيله لاحقاً):
-  // getIt.registerSingleton<AudioHandler>(await initAudioService());
+  // تسجيل مستعلم الملفات الصوتية
+  getIt.registerLazySingleton<OnAudioQuery>(() => OnAudioQuery());
 }
--- lib/presentation/pages/songs_page.dart
+++ lib/presentation/pages/songs_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:get_it/get_it.dart';
 
 class SongsPage extends StatefulWidget {
   const SongsPage({super.key});
@@ -9,7 +11,19 @@ class SongsPage extends StatefulWidget {
 }
 
 class _SongsPageState extends State<SongsPage> {
-  final OnAudioQuery _audioQuery = OnAudioQuery();
+  final OnAudioQuery _audioQuery = GetIt.I<OnAudioQuery>();
+  final AudioPlayer _audioPlayer = GetIt.I<AudioPlayer>();
+
+  Future<void> _playSong(SongModel song) async {
+    try {
+      Uri audioUri = Uri.parse("content://media/external/audio/media/${song.id}");
+
+      await _audioPlayer.setAudioSource(AudioSource.uri(audioUri));
+      await _audioPlayer.play();
+    } catch (e) {
+      debugPrint("Error Playing Song: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +33,8 @@ class _SongsPageState extends State<SongsPage> {
           padding: const EdgeInsetsGeometry.symmetric(horizontal: 10),
           sliver: FutureBuilder<List<SongModel>>(
             future: _audioQuery.querySongs(
-              sortType: null, // No Sort
-              orderType: OrderType.ASC_OR_SMALLER, // Ascindent
+              sortType: SongSortType.DATE_ADDED,
+              orderType: OrderType.DESC_OR_GREATER,
               uriType: UriType.EXTERNAL, // Look For External Storage
               ignoreCase: true, // Ignore Letter Cases
             ),
@@ -46,9 +60,7 @@ class _SongsPageState extends State<SongsPage> {
                     subtitle: Text(song.artist ?? "Unknown Artist", style: Theme.of(context).textTheme.bodySmall, maxLines: 1, overflow: TextOverflow.ellipsis),
                     // Song Cover (ArtWork)
                     leading: QueryArtworkWidget(id: song.id, type: ArtworkType.AUDIO, nullArtworkWidget: const Icon(Icons.music_note)),
-                    onTap: () {
-                      print("Playing ${song.title}");
-                    },
+                    onTap: () => _playSong(song),
                   );
                 }, childCount: item.data!.length),
               );
