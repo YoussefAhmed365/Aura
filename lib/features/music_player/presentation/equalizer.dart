6257448b7bc482670d06fb3354284210a428c24d
YoussefAhmed365 <youssef.ahmed5002@gmail.com>
2026-02-08 21:17:20 +0200

    Initial Commit

---
+++ lib/features/music_player/presentation/equalizer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EqualizerScreen extends StatelessWidget {
+  const EqualizerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text("Equalizer")
+    );
+  }
+}
